66197333e045a3223164094e22a9d3c84feda262
JJ Garlitos <acoustic_ninja@hotmail.com>
2019-09-17 22:05:53 -0600

    Added Sizes to Drinks

--- Models/Drink.cs
+++ Models/Drink.cs
@@ -2,11 +2,18 @@ using BurgerShack.Interfaces;
 
 namespace BurgerShack.Models
 {
+    public enum DrinkSizes
+    {
+        Small = 1,
+        Medium,
+        Large
+    }
     public class Drink : IItem
     {
         public string Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public DrinkSizes Size { get; set; }
     }
 }
